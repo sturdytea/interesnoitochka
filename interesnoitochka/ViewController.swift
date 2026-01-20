a1b2893172fe14f9c4b618e171a961b392d10d11
sturdytea <aitmazh@gmail.com>
2026-01-20 15:28:11 +0500

    first commit

---
+++ interesnoitochka/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//
+// ViewController.swift
+// interesnoitochka
+//
+// Created by sturdytea on 20.01.2026.
+//
+// GitHub: https://github.com/sturdytea
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
